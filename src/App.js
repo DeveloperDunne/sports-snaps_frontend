c0cf7503750e5da50c42847e8da69e7a6a3bb961
Carl Dunne <carl.m.dunne@hotmail.com>
2024-07-30 15:49:02 +0000

    Renamed File

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import "./api/axios.Defaults";
-import SignUpForm from "./pages/SignUpForm";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -12,9 +12,8 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} >
-          <Route exact path="/signup" render={() => <SignUpForm />}
-          <Route exact path="/contact" render={() => <h1>Contact Us</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://sportssnaps-api-4f584f2a4ad4.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
